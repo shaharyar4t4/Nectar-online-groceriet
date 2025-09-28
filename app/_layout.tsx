a2592053cc122e86b2e5dc9bed337a8f568e2288
Shaharyar <shaharyar4t4@gmail.com>
2025-09-28 18:18:44 +0500

    add the theme setup

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,47 @@
-import { View } from "react-native";
+import { darkTheme, lightTheme } from '@/theme/theme';
+import { useFonts } from 'expo-font';
+import { StyleSheet, Text, View } from "react-native";
+import { PaperProvider } from 'react-native-paper';
+
 
 export default function RootLayout() {
-  
+ 
+  const isDarktheme = true;
+  // hook for font family 
+  const [fontLoaded] = useFonts({
+    "Gilroy-Bold": require("../assets/fonts/Gilroy-Bold.ttf"),
+    "Gilroy-Light": require("../assets/fonts/Gilroy-Light.ttf"),
+    "Gilroy-Medium": require("../assets/fonts/Gilroy-Medium.ttf"),
+    "Gilroy-Regular": require("../assets/fonts/Gilroy-Regular.ttf"),
+  })
+  if (!fontLoaded) {
+    return <Text></Text>
+    // this loaded is help if some case the font is not loaded so we show the Empty screen
+  }
+
+ 
+
 
   return (
-   <View>
+    <PaperProvider theme={isDarktheme ? darkTheme : lightTheme}>
+      <View style={styles.container}>
+      <Text style={styles.txt}>Hello World</Text>
+    </View>
+    </PaperProvider>
     
-   </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: darkTheme.colors.background,
+  },
+  txt: {
+    fontSize: 24,
+    fontFamily: "Gilroy-Medium",
+    color: darkTheme.colors.primary,
+  }
+}); 
---
+++ theme/theme.tsx
@@ -0,0 +1,18 @@
+import {
+    MD3DarkTheme,
+    MD3LightTheme
+} from 'react-native-paper';
+
+//show the light theme
+export const lightTheme ={
+    ...MD3LightTheme,
+    primary: "#E21B70",
+    background: "#FEFEFE"
+}
+
+// show the dark theme
+export const darkTheme ={
+    ...MD3DarkTheme,
+    primary: "#E21B70",
+    background: "#E0E0E0"
+}
